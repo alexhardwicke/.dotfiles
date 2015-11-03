ed07ac72182446386f8b100daf0521f2d82ae400
Alex Hardwicke <alex.hardwicke@erisma.se>
2015-11-03 08:32:42 +0100

    Don't newline in git repos

--- .git-prompt-colors.sh
+++ .git-prompt-colors.sh
@@ -7,8 +7,8 @@ override_git_prompt_colors() {
   GIT_PROMPT_STAGED="${Yellow}●"
   GIT_PROMPT_STASHED="${BoldMagenta}▼ "
   GIT_PROMPT_CLEAN="${Green}✔"
-  GIT_PROMPT_END_USER=" \n$ "
-  GIT_PROMPT_END_ROOT=" \n# "
+  GIT_PROMPT_END_USER=" $ "
+  GIT_PROMPT_END_ROOT=" # "
   GIT_PROMPT_START_USER="${Blue}${PathShort}${ResetColor}"
   GIT_PROMPT_START_ROOT="${GIT_PROMPT_START_USER}"
 }
